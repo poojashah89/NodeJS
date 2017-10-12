5e551730cd07d9239a23ccff90741f270ac437a9
Pooja <shah.pooja89@gmail.com>
2017-10-11 23:22:03 -0700

    Update auth.js

--- ampere_health_app-master/config/auth.js
+++ ampere_health_app-master/config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
     'fitbitAuth' : {
-        'clientID'      : '227XHX', // your App ID
-        'clientSecret'  : 'e6cd00b32b5570a445dd9a7048e05bfe', // your App Secret
+        'clientID'      : '', // your App ID
+        'clientSecret'  : '', // your App Secret
         'callbackURL'   : 'http://localhost:3000/auth/fitbit/callback'
     }
 };
